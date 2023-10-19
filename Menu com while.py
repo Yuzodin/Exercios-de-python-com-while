5265410c0d0394ff02e0f05615ad75d0c0c871a0
Yuri <140124162+Yuzodin@users.noreply.github.com>
2023-10-19 08:14:14 -0300

    Add files via upload
    Um programa que le dois valores e mostre um menu na tela:
    
    [ 1 ] somar
    [ 2 ] multiplicar
    [ 3 ] maior número
    [ 4 ] novos números
    [ 5 ] sair do programa
    utilizando while e if.

---
+++ Menu com while.py
@@ -0,0 +1,31 @@
+from time import sleep
+n1 = int (input('Digite o 1o numero: '))
+n2 = int (input ('Digite o 20 numero: '))
+op = 0
+while op != 5:
+    print('''====== MENU ======
+          [1] SOMA
+          [2] MULTIPLICAÇÃO
+          [3] MAIOR NUMERO
+          [4] NOVOS NUMEROS
+          [5] SAIR DO PROGAMA ''')
+    op = int(input ('Digite sua opção: '))
+    if op == 1:
+        print(f'A soma entre: {n1} + {n2} = {n1 + n2}')
+    elif op == 2:
+        print(f'A multiplicação entre: {n1} x {n2} = {n1 * n2}')
+    elif op == 3:
+        if n1 > n2:
+            print(f'O maior numero entre os dois foi: {n1}')
+        else:
+            print(f'O maior numero entre os dois foi: {n2}')
+    elif op == 4:
+        print('Digite os novos valores:')
+        n1 = int (input('Digite o 1o numero: '))
+        n2 = int (input ('Digite o 20 numero: '))
+    elif op == 5:
+        print('Saindo do progama!!') 
+        sleep(2)  
+    else:
+        print('Opção invalida tente novamnete: ')             
+print('Fim do progama!.')    
